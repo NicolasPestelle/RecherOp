841c743a5991c30d0c5136a6e6bd0f5cd9409250
Nicolas <nicolas.pestelle@hotmail.fr>
2017-10-20 16:00:16 +0200

    avancé sacADos

--- Sac.cpp
+++ Sac.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include "Sac.hpp"
+#include <sstream>
+
 using namespace std;
 
 Sac::Sac(int poidMax) : _poidMax(poidMax){}
@@ -10,11 +12,12 @@ void Sac::RempliObjetPossible(Objet o){
 }
 
 string Sac::AfficheObjetPossible(){
-
+stringstream ss;
   string tt ;
   for (int i = 0; i < _ObjetPossible.size();i++){
-    tt +=  "Objet num : " _ObjetPossible[i] " poid Objet : "  _ObjetPossible[i].getPoid() " profit objet : " _ObjetPossible[i].getProfit();
+    ss +=  "Objet num : " << (string)_ObjetPossible[i] << " poid Objet : " << (string)_ObjetPossible[i].getPoid()<< " profit objet : "<< (string)_ObjetPossible[i].getProfit();
   }
-  return tt;
+  
+  return tt = ss.str();
   
 }
